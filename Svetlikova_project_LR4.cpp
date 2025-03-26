48466763b6c4aaa6ee79d7f153fe7cc71155dbed
svetlikovalv <svetlikova13@gmail.com>
2025-03-26 21:07:42 +0300

    Revert " base functions added"
    This reverts commit 8cd5d1bd933a3e1323e76979f9b5a3b96ffd6a89.

--- Svetlikova_project_LR4.cpp
+++ Svetlikova_project_LR4.cpp
@@ -2,51 +2,20 @@
 #include <map>
 #include <string>
 #include <functional>
-#include <cmath>
 
 using namespace std;
 
 double a;
 double b;
 
-
-
-bool user_input(string input){
-    if (input.empty())return false;
-    try{
-        double number=stod(input);
-        
-    }
-    catch(...)
-    {return false;}
-    return true;
+function<void()> enter_number(double&var_link,string label){
+    //branch_fun_1
 }
-
-
-
-function<void()>enter_choice(int&var_link,string label){
-    return[&var_link,label](){
-        string raw_input;
-        cout<<label<<" = ";
-        getline(cin,raw_input);
-        while (!user_input(raw_input))
-        {
-            cout<<label<<" = ";
-            getline(cin,raw_input);
-
-        }
-        var_link=stoi(raw_input);
-    };
-
-}
-//branch_func_1
-function<void()>enter_number(double&var_link,string label){}
-
 void modul(){
-    //branch_func_2
+    //branch_fun_2
 }
 void argument(){
-    //branch_func_3
+    //branch_fun_3
 }
 struct menu_item{
     string title;
@@ -55,37 +24,34 @@ struct menu_item{
 int main(){
     
 
-    map<int, menu_item> menu = {
+    map<int, menu_item > menu = {
         {1,{"Bвод вещественной части комплексного числа - a",enter_number(a,"введите целую часть комсплексного числа - а")}},
-        {2,{"Bвод мнимой части комплексного числа - b",enter_number(b,"введите целую часть комсплексного числа - b")}},
+        {2,{"Bвод мнимой части комплексного числа - b",enter_number(b,"введите мнимую часть комсплексного числа - b")}},
         {3,{"Найти модуль комплексного числа",modul}},
         {4,{"Найти аргумент комплексного числа",argument}},
         
     };
-    
+    double choice = 0;
 
-    int choice = 0;
-
-    while (true) {
-        cout << "Меню:" <<endl;
-        for (const auto& item : menu) {
-            cout << "Task " << item.first << ". " << item.second.title << endl;
-        }
-        cout << "0. Выход" << std::endl;
-        cout << "Введите номер пункта: ";
-        cin >> choice;
-
-        if (choice == 0) {
-            cout << "Вы вышли из программы" << endl;
+    cout<<"Меню: "<<endl;
+    for (const auto& item : menu){
+        cout<<"Task "<<item.first<<". "<<item.second.title<<endl;
+    }
+    cout<<"0. Выход"<<endl;
+    while (true){
+        enter_number(choice,"Введите номер задачи: ")();
+        if (choice==0){
+            cout<<"Вы вышли из программы"<<endl;
             break;
         }
-        cout << endl;
-        if (menu.find(choice) != menu.end()) {
-            menu[choice].action();
-        } else {
-            cout << "Некорректный ввод.";
+        cout<<"==========Action=========="<<endl;
+        if (menu.find(choice)!=menu.end()){
+            menu[choice].action;
         }
-        cout << endl ;
+        else{
+            cout<<"Некорректный ввод. ";
+        }
+        cout<<endl;
     }
     return 0;
 }
