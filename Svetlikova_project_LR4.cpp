5f442c1114a3d54fcb2611f317c263d168d088ef
svetlikovalv <svetlikova13@gmail.com>
2025-03-26 21:18:28 +0300

    branch_func_3 argument function added

--- Svetlikova_project_LR4.cpp
+++ Svetlikova_project_LR4.cpp
@@ -2,11 +2,39 @@
 #include <map>
 #include <string>
 #include <functional>
+#include <cmath>
 
 using namespace std;
 
 double a;
 double b;
+
+bool user_input(string input){
+    if (input.empty())return false;
+    try{
+        double number=stod(input);
+        
+    }
+    catch(...)
+    {return false;}
+    return true;
+}
+
+function<void()>enter_choice(int&var_link,string label){
+    return[&var_link,label](){
+        string raw_input;
+        cout<<label<<" = ";
+        getline(cin,raw_input);
+        while (!user_input(raw_input))
+        {
+            cout<<label<<" = ";
+            getline(cin,raw_input);
+
+        }
+        var_link=stoi(raw_input);
+    };
+
+}
 
 function<void()> enter_number(double&var_link,string label){
     //branch_fun_1
@@ -16,6 +44,23 @@ void modul(){
 }
 void argument(){
     //branch_fun_3
+    cout<<"комплексная часть числа а= "<<a<<"  комплексная часть числа b= "<<b<<endl;
+    if (a>0){
+        cout<<"аргумент функции"<<atan2(b,a)<<endl;
+    }
+    if (a<0 && b>0){
+        cout<<"аргумент функции"<<atan2(b,a)+acos(-1.0) <<endl;
+    }if(a<0 && b<0){ 
+        cout<<"аргумент функции"<<atan2(b,a)-acos(-1.0) <<endl;
+    }if(a==0&&b>0){
+        cout<<"аргумент функции"<<acos(-1.0)/2<<endl;
+    }
+    if(a==0&&b<0){
+        cout<<"аргумент функции"<<- acos(-1.0)/2 <<endl;
+    }if (a==0 && b==0){
+        cout<<"аргумента не существует"<<endl;
+    }
+
 }
 struct menu_item{
     string title;
@@ -26,32 +71,33 @@ int main(){
 
     map<int, menu_item > menu = {
         {1,{"Bвод вещественной части комплексного числа - a",enter_number(a,"введите целую часть комсплексного числа - а")}},
-        {2,{"Bвод мнимой части комплексного числа - b",enter_number(b,"введите мнимую часть комсплексного числа - b")}},
+        {2,{"Bвод мнимой части комплексного числа - b",enter_number(b,"введите целую часть комсплексного числа - b")}},
         {3,{"Найти модуль комплексного числа",modul}},
         {4,{"Найти аргумент комплексного числа",argument}},
         
     };
-    double choice = 0;
+    int choice = 0;
 
-    cout<<"Меню: "<<endl;
-    for (const auto& item : menu){
-        cout<<"Task "<<item.first<<". "<<item.second.title<<endl;
-    }
-    cout<<"0. Выход"<<endl;
-    while (true){
-        enter_number(choice,"Введите номер задачи: ")();
-        if (choice==0){
-            cout<<"Вы вышли из программы"<<endl;
+    while (true) {
+        cout << "Меню:" <<endl;
+        for (const auto& item : menu) {
+            cout << "Task " << item.first << ". " << item.second.title << endl;
+        }
+        cout << "0. Выход" << std::endl;
+        cout << "Введите номер пункта: ";
+        cin >> choice;
+
+        if (choice == 0) {
+            cout << "Вы вышли из программы" << endl;
             break;
         }
-        cout<<"==========Action=========="<<endl;
-        if (menu.find(choice)!=menu.end()){
-            menu[choice].action;
+        cout << endl;
+        if (menu.find(choice) != menu.end()) {
+            menu[choice].action();
+        } else {
+            cout << "Некорректный ввод.";
         }
-        else{
-            cout<<"Некорректный ввод. ";
-        }
-        cout<<endl;
+        cout << endl ;
     }
     return 0;
 }
