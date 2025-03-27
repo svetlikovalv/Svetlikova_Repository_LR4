f365162e90cbe720ddff8e1ee1960435b3141240
svetlikovalv <svetlikova13@gmail.com>
2025-03-27 21:51:13 +0300

    Update Svetlikova_project_LR4.cpp
    исправление повторов кода

--- Svetlikova_project_LR4.cpp
+++ Svetlikova_project_LR4.cpp
@@ -4,40 +4,10 @@
 #include <functional>
 #include <cmath>
 
-#include <cmath>
-
-
-
 using namespace std;
 
 double a;
 double b;
-bool user_input(string input){
-    if (input.empty())return false;
-    try{
-        double number=stod(input);
-        
-    }
-    catch(...)
-    {return false;}
-    return true;
-}
-
-function<void()>enter_choice(int&var_link,string label){
-    return[&var_link,label](){
-        string raw_input;
-        cout<<label<<" = ";
-        getline(cin,raw_input);
-        while (!user_input(raw_input))
-        {
-            cout<<label<<" = ";
-            getline(cin,raw_input);
-
-        }
-        var_link=stoi(raw_input);
-    };
-
-}
 
 bool user_input(string input){
     if (input.empty())return false;
@@ -121,7 +91,7 @@ int main(){
 
     map<int, menu_item > menu = {
         {1,{"Bвод вещественной части комплексного числа - a",enter_number(a,"введите целую часть комсплексного числа - а")}},
-        {2,{"Bвод мнимой части комплексного числа - b",enter_number(b,"введите целую часть комсплексного числа - b")}},
+        {2,{"Bвод мнимой части комплексного числа - b",enter_number(b,"введите мнимую часть комсплексного числа - b")}},
         {3,{"Найти модуль комплексного числа",modul}},
         {4,{"Найти аргумент комплексного числа",argument}},
         
